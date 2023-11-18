d6c51efe45d5085d1921bac00f32080b04eb0d77
Ebere <eiweala@live.com>
2023-11-18 05:15:09 -0800

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import CitySearch from "./components/CitySearch";
 import Eventlist from "./components/Eventlist";
 import NumberOfEvents from "./components/NumberOfEvents";
@@ -9,35 +9,20 @@ const App = () => {
   const [selectedCity, setSelectedCity] = React.useState("See all cities");
   const [allLocations, setAllLocations] = React.useState([]);
   const [events, setEvents] = React.useState([]);
-  const [eventNumber, setEventNumber] = useState(32);
   const [filteredEvents, setFilteredEvents] = React.useState([]);
   const [noOfEvents, setNoOfEvents] = React.useState(32);
   const [errorAlert, setErrorAlert] = React.useState("");
-  const [currentNOE, setCurrentNOE] = useState(32);
-
-  useEffect(() => {
-    fetchData();
-  }, [selectedCity]);
-
-  const fetchData = async () => {
-    const allEvents = await getEvents();
-    const filteredEvents = selectedCity === 'See all cities' ?
-    allEvents :
-    allEvents.filter(event => event.location === selectedCity)
-    setEvents(filteredEvents.slice(0, currentNOE));
-    setAllLocations(extractLocations(allEvents));
-  }
 
   React.useEffect(() => {
-    (async () => {
-      const allEvents = await getEvents(); // in the future this should be replaced with an api call
-      setEvents(allEvents);
+    const fetchData = async () => {
+      const allEvents = await getEvents();
+      setEvents(allEvents.slice(0, 32));
       setAllLocations(extractLocations(allEvents));
-    })();
+    };
+    fetchData();
   }, []);
 
   function onEventNumberChange(value, number) {
-    setEventNumber(number);
     setNoOfEvents(value);
     handleCitySelected(selectedCity, value, number);
   }
@@ -55,13 +40,13 @@ const App = () => {
       sliced = filteredEvents.slice(0, numberOfEvents);
     }
     setFilteredEvents(sliced);
-  };
+  }
 
   return (
     <div className="App">
       <CitySearch
         allLocations={allLocations}
-        setSelectedCity={setSelectedCity}
+        setSelectedCity={(val) => handleCitySelected(val, noOfEvents)}
       />
       {/* If the number changes and the event filter is filled */}
       <Eventlist events={filteredEvents.length > 0 ? filteredEvents : events} />
